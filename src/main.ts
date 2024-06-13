567dc913251d178dcfe9ab4f5fb8a34a357bda98
mariobros123413 <j.mario18@hotmail.es>
2024-06-13 00:10:50 -0400

    cors?

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json());
   app.enableCors();
-  const cors = require('cors');
-  app.use(cors({
-    origin: '*',
-    credentials: true,
-  }));
   // app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
